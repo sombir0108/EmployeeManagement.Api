c73fcd3f6f98357c4ee72252d74d7587e7729f94
priyagupta123 <priyagpt705@gmail.com>
2022-05-30 20:52:46 +0530

    configure SQL Server

---
+++ Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement.Api
+{
+    internal class Configuration
+    {
+        internal static object GetConnectionString(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Api
+{
+    
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+
+        }
+
+        public IConfiguration configuration1 { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer((string)Configuration.GetConnectionString("DBConnection")));
+
+            services.AddControllers();
+        }
+    }
+}
